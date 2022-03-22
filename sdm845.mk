5aa2ec59513849108547bf1171efa08bdfe6c4c2
Dhruv <dhruvgera61@gmail.com>
2022-03-22 16:52:19 +0100

    beryllium: Nuke libperfmgr

--- sdm845.mk
+++ sdm845.mk
@@ -377,11 +377,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-sdm845-libperfmgr \
     android.hardware.power.stats@1.0-service.xiaomi
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
